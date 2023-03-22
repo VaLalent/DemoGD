12e55b10c7053c47c06f5950f2e6b983184cefc5
VaLalent <108317198+VaLalent@users.noreply.github.com>
2023-03-22 21:04:54 +0700

    Add animation and Unit selected

--- demoGameDesign/Assets/Script/Unit.cs
+++ demoGameDesign/Assets/Script/Unit.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
+
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
+
     private void Update()
     {
+
         float stoppingDistance = .1f;
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            unitAnimator.SetBool("isWalk", true);
+
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+        }
+        else
+        {
+            unitAnimator.SetBool("isWalk", false);
         }
 
-        if (Input.GetMouseButtonDown(1))
-        {
-            Move(MouseWorld.GetPosition());
-        }
     }
-    private void Move(Vector3 targetPosition)
+    public void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
     }
